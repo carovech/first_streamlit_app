55b22ca4128ef41ea12c36a054272a8dc8fd484a
Caro <70977956+carovech@users.noreply.github.com>
2023-01-03 13:06:28 +0100

    Update streamlit_app.py
    begin to add snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,6 @@ streamlit.dataframe(my_data_rows)
 #Add a  second Text Entry Box to allow user to add fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+# add snowflake part
+my_cure.execute("insert into fruit_load_list values ('from streamlit')")
